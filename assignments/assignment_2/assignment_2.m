bd18093096ac44c507e6e67dfa78f9880b2f74e1
Charles Arnaudo <charlesarnaudo@gmail.com>
2019-01-30 13:29:54 -0500

    problem 3

--- assignments/assignment_2/assignment_2.m
+++ assignments/assignment_2/assignment_2.m
@@ -3,62 +3,89 @@
     arnaudoc@wit.edu
     Assignment 2
 %}
-clear all;
-clearvars;
+ clear all;
+ clearvars;
 
-disp("---Finish Solving Problem 1a---");
-pause;
+ disp("---Finish Solving Problem 1a---");
+ pause;
 
-% A
-A = imread("Food.jpg");
-B = A;
-B = imadjust(B, [0.35 0.65], [0 1]);
+ % A
+ A = imread("Food.jpg");
+ B = A;
+ B = imadjust(B, [0.35 0.65], [0 1]);
 
-figure;
-subplot(1,2,1);
-imshow(A);
-title("Non adjusted image");
-subplot(1,2,2);
-imshow(B);
-title("Adjusted Image");
-imwrite(B, "ScaledFood.jpg");
+ figure;
+ subplot(1,2,1);
+ imshow(A);
+ title("Non adjusted image");
+ subplot(1,2,2);
+ imshow(B);
+ title("Adjusted Image");
+ imwrite(B, "ScaledFood.jpg");
 
-disp("---Finish Solving Problem 1b---");
-pause;
+ disp("---Finish Solving Problem 1b---");
+ pause;
 
-subplot(1,2,1);
-imhist(A);
-title("Histogram");
-subplot(1,2,2);
-h = histogram(A, 'Normalization', 'probability');
-title("Normalized Histogram");
+ subplot(1,2,1);
+ imhist(A);
+ title("Histogram");
+ subplot(1,2,2);
+ h = histogram(A, 'Normalization', 'probability');
+ title("Normalized Histogram");
 
-disp("---Finish Solving Problem 1c---");
-pause;
+ disp("---Finish Solving Problem 1c---");
+ pause;
 
-A = imread("Food.jpg");
-B = imread("ScaledFood.jpg");
-C = histeq(A);
-imwrite(C, "EqualizedFood.jpg");
+ A = imread("Food.jpg");
+ B = imread("ScaledFood.jpg");
+ C = histeq(A);
+ imwrite(C, "EqualizedFood.jpg");
 
-subplot(1,3,1);
-imshow(A);
-title("Original");
-subplot(1,3,2);
-imshow(B);
-title("Adjusted Image");
-subplot(1,3,3);
-imshow(C);
-title("Equalized image")
+ subplot(1,3,1);
+ imshow(A);
+ title("Original");
+ subplot(1,3,2);
+ imshow(B);
+ title("Adjusted Image");
+ subplot(1,3,3);
+ imshow(C);
+ title("Equalized image")
 
-disp("---Finish Solving Problem 1d---");
-pause;
+ disp("---Finish Solving Problem 1d---");
+ pause;
 
-close all;
-clear all;
+ close all;
+ clear all;
+ disp("---Finish Solving Problem 2---");
+ pause;
 
-disp("---Finish Solving Problem 2---");
-pause;
+ figure;
+ A = imread("Shapes.tiff");
 
+ subplot(2, 2, 1);
+ imshow(A);
+ title("Original");
 
+ subplot(2, 2, 2);
+ se = strel('rectangle', [20, 20]);
+ B = imopen(A, se);
+ imshow(B);
+ title("Opened");
 
+ subplot(2, 2, 3);
+ se = strel('rectangle', [20, 20]);
+ C = imclose(A, se);
+ imshow(C);
+ title("Closed");
+
+ subplot(2, 2, 4);
+ se = strel('rectangle', [20, 20]);
+ D = imopen(A, se);
+ D = imdilate(D, se);
+ imshow(D);
+ title("Open and then dilated");
+
+ disp("---Finish Solving Problem 3---");
+ pause;
+ close all;
+ clear all;
