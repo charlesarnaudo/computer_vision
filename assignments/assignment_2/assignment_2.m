0f4e6ff543cdcc4bcb639e4df2727d62ea56afd8
Charles Arnaudo <charlesarnaudo@gmail.com>
2019-01-30 10:35:39 -0500

    problem one

--- assignments/assignment_2/assignment_2.m
+++ assignments/assignment_2/assignment_2.m
@@ -9,18 +9,47 @@ clearvars;
 disp("---Finish Solving Problem 1---");
 pause;
 
+% A
 A = imread("Food.jpg");
 B = A;
 B = imadjust(B, [0.35 0.65], [0 1]);
 
+figure;
 subplot(1,2,1);
 imshow(A);
 title("Non adjusted image");
 subplot(1,2,2);
 imshow(B);
 title("Adjusted Image");
+imwrite(B, "ScaledFood.jpg");
 
-imwrite(B, "Food_Adjusted.jpg");
+% B
+figure;
+subplot(1,2,1);
+imhist(A);
+title("Histogram");
+subplot(1,2,2);
+h = histogram(A, 'Normalization', 'probability');
+title("Normalized Histogram");
+
+% C
+A = imread("Food.jpg");
+B = imread("ScaledFood.jpg");
+C = histeq(A);
+imwrite(C, "EqualizedFood.jpg");
+
+figure;
+subplot(1,3,1);
+imshow(A);
+title("Original");
+subplot(1,3,2);
+imshow(B);
+title("Adjusted Image");
+subplot(1,3,3);
+imshow(C);
+title("Equalized image")
 
 disp("---Finish Solving Problem 2---");
 pause;
+close all;
+clear all;
