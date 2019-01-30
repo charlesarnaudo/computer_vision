5d7012e9fdaa4e0fc5387318b5166554e3c201db
Charles Arnaudo <charlesarnaudo@gmail.com>
2019-01-30 12:59:31 -0500

    duh

--- assignments/assignment_2/assignment_2.m
+++ assignments/assignment_2/assignment_2.m
@@ -6,7 +6,7 @@
 clear all;
 clearvars;
 
-disp("---Finish Solving Problem 1---");
+disp("---Finish Solving Problem 1a---");
 pause;
 
 % A
@@ -23,8 +23,9 @@ imshow(B);
 title("Adjusted Image");
 imwrite(B, "ScaledFood.jpg");
 
-% B
-figure;
+disp("---Finish Solving Problem 1b---");
+pause;
+
 subplot(1,2,1);
 imhist(A);
 title("Histogram");
@@ -32,13 +33,14 @@ subplot(1,2,2);
 h = histogram(A, 'Normalization', 'probability');
 title("Normalized Histogram");
 
-% C
+disp("---Finish Solving Problem 1c---");
+pause;
+
 A = imread("Food.jpg");
 B = imread("ScaledFood.jpg");
 C = histeq(A);
 imwrite(C, "EqualizedFood.jpg");
 
-figure;
 subplot(1,3,1);
 imshow(A);
 title("Original");
@@ -49,7 +51,14 @@ subplot(1,3,3);
 imshow(C);
 title("Equalized image")
 
-disp("---Finish Solving Problem 2---");
+disp("---Finish Solving Problem 1d---");
 pause;
+
 close all;
 clear all;
+
+disp("---Finish Solving Problem 2---");
+pause;
+
+
+
