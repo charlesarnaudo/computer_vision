dc2586533639a0da5023b102fce029ba366195bb
Charles Arnaudo <charlesarnaudo@gmail.com>
2019-02-07 08:43:07 -0500

    finish problem 1

--- assignments/assignment_3/assignment_3.m
+++ assignments/assignment_3/assignment_3.m
@@ -17,9 +17,11 @@ figure;
 subplot(1,3,1);
 imshow(A);
 title("Original")
+
 subplot(1, 3, 2);
 imshow(B);
 title("3x3")
+
 subplot(1, 3, 3)
 imshow(C);
 title("5x5")
@@ -30,15 +32,24 @@ close all;
 clear all;
 
 A = imread('Circuit.jpg');
-h = fspecial('average', [3 3]);
-B = filter2(h, A);
 
 subplot(1,3,1);
 imshow(A);
 title("Original")
+
 subplot(1, 3, 2);
+h = fspecial('average', [3 3]);
+B = filter2(h, A)/255;
 imshow(B);
 title("3x3")
+
+subplot(1, 3, 3);
+h = fspecial('average', [5 5]);
+C = filter2(h, A)/255;
+imshow(C);
+title("5x5")
+
+disp("The results are not the same as mine process");
 
 function [output] = MeanFilter(image, filterSize)
 %Mean filter
@@ -51,8 +62,13 @@ function [output] = MeanFilter(image, filterSize)
     for i=1+buffer:row-buffer
         for j=1+buffer:column-buffer
             avg = 0;
-            avg = image(i-buffer, j-buffer) + image(i-buffer, j) + image(i-buffer, j+buffer) + image(i, j) + image(i, j-buffer) + image(i, j+buffer) + image(i+buffer, j-buffer) + image(i+buffer, j) + image(i+buffer, j+buffer);
-            output(i, j) = avg / 9;
+            for y=-buffer:buffer
+                for z=-buffer:buffer
+                    avg = avg + image(i+y, j+z);
+                end
+            end
+            % avg = image(i-buffer, j-buffer) + image(i-buffer, j) + image(i-buffer, j+buffer) + image(i, j) + image(i, j-buffer) + image(i, j+buffer) + image(i+buffer, j-buffer) + image(i+buffer, j) + image(i+buffer, j+buffer);
+            output(i, j) = avg / filterSize^2;
         end
     end
 end
